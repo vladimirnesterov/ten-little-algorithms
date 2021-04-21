c730aeaf53812663cfbda94c32cfdbb7ddeae444
vladimirnesterov <69502306+vladimirnesterov@users.noreply.github.com>
2021-04-22 00:11:52 +0900

    Add Newton's method for finding roots

--- examples.py
+++ examples.py
@@ -7,24 +7,47 @@ Examples for ten little algorithms
 
 import ten_little_algorithms as tla
 
-print('')
-print('  --- ----------------------- *** ----------------------- ---  ')
-print('   Euclidean Algorithm to find greatest common divider (GCD)   ')
-print('')
+print("")
+print("  --- ----------------------- *** ----------------------- ---  ")
+print("   Euclidean Algorithm to find greatest common divider (GCD)   ")
+print("")
 from math import gcd
 a=1071
 b=462
-print('Python gcd function gcd(', a,',', b, ') =',gcd(a,b))
-print('Euclidean algorithm gcd(', a,',', b, ') =',tla.euclidean_gcd(a,b))
-print('')
+print("Python gcd function gcd(", a,",", b, ") =",gcd(a,b))
+print("Euclidean algorithm gcd(", a,",", b, ") =",tla.euclidean_gcd(a,b))
+print("")
 
-print('  --- ----------------------- *** ----------------------- ---  ')
-print(' Extended Euclidean Algorithm to find GCD and Bézouts identity ')
-print('')
+print("  --- ----------------------- *** ----------------------- ---  ")
+print(" Extended Euclidean Algorithm to find GCD and Bézout's identity ")
+print("")
 a=462
 b=1071
 gdc,x,y = tla.euclidean_ext_gcd(a,b)
-print('Euclidean algorithm gcd(', a,',', b, ') =',gdc)
-print('Coefficients of Bézouts identity:',x, 'and', y)
-print('such that ',a, '*', x, '+', b, '*', y, '=', a*x+b*y)
-print('')
+print("Euclidean algorithm gcd(", a,",", b, ") =",gdc)
+print("Coefficients of Bézout's identity:",x, "and", y)
+print("such that ",a, "*", x, "+", b, "*", y, "=", a*x+b*y)
+print("")
+
+print("  --- ----------------------- *** ----------------------- ---  ")
+print("              Newton's method for finding roots                ")
+print("")
+from scipy import optimize
+
+def f(x: float) -> float:
+    return x**2 - 20 * x + 5
+
+def f_derivative(x: float) -> float:
+    return 2 * x - 20
+
+x0 = 100 #1
+eps = 1e-7
+kmax =1e3
+
+newton_result = tla.newton(f, f_derivative, x0, eps, kmax)    
+scipy_result = optimize.root(f, x0)
+
+print("Test function is (x^2 - 20*x + 5), first guess is", x0)
+print("Newton's method for finding root =",newton_result, "f(newton root)=", f(newton_result))
+print("Scipy function for finding roots =",scipy_result.x[0], "f(scipy root)= ", f(scipy_result.x[0]))
+print("")
--- ten_little_algorithms.py
+++ ten_little_algorithms.py
@@ -12,8 +12,8 @@ def euclidean_gcd(a,b):
     the largest number that divides them both without a remainder [Wiki].
 
     Args:
-        a (int): The first integer.
-        b (int): The second integer.
+        a (int): The first integer, > 0,
+        b (int): The second integer, > 0.
 
     Returns:
         int: the greatest common divisor.
@@ -26,6 +26,7 @@ def euclidean_gcd(a,b):
         a = a - b
         
     if (a != b):
+        #print("a =", a, "b =", b)
         a = euclidean_gcd(b, a)
         
     return a
@@ -40,8 +41,8 @@ def euclidean_ext_gcd(a,b):
     x and y such that ax+by = gcd(a,b) [Wiki].
 
     Args:
-        a (int): The first integer.
-        b (int): The second integer.
+        a (int): The first integer, > 0,
+        b (int): The second integer, > 0.
 
     Returns:
         tuple(int,int,int): the gcd and coefficients x and y.
@@ -75,6 +76,37 @@ def euclidean_ext_gcd(a,b):
         flip = 1
     
     while (b != 0):
+        #print("a =", a, "b =", b, "s =", s, "t =", t)
         a,b,s,spv,t,tpv = calc_next_step(a,b,s,spv,t,tpv)
         
     return (a,tpv,spv) if flip else (a,spv,tpv)
+
+def newton(f, f_derivative, x0, eps, kmax):
+    """Newton's method for finding roots.
+    
+    The Newton's method (Newton–Raphson method) is a root-finding algorithm 
+    which produces approximations to the roots (or zeroes) 
+    of a real-valued function. [Wiki].
+
+    Args:
+        f (function): single-variable function f ,
+        f_derivative (function):  the function's derivative f ′,
+        x0 (float): initial guess,
+        eps (float): precision wanted,
+        kmax (int): maximum number of iterations.
+
+    Returns:
+        x (float): root of f(x) = 0.
+
+    """
+    x = x0
+    x_prev = x0 + 2 * eps
+    i = 0
+	
+    while (abs(x - x_prev) >= eps) and (i < kmax):
+        #print("Step", i, ":", int(x), int(x_prev), ", x - f(x) = ", int(x - f(x)), ", f_derivative(x) = ", int(f_derivative(x)), "f/f'=",int(f(x)/f_derivative(x)))
+        x, x_prev =  x - ( f(x) / f_derivative(x) ), x
+        i += 1
+        
+
+    return x
